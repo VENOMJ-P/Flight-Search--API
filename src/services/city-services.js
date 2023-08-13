032f54b9618d3dd98bf151b9a2a7d31fe584f769
VENOMJ-P <jay63837@gmail.com>
2023-08-13 15:47:37 +0530

    Added city Services

---
+++ src/services/city-services.js
@@ -0,0 +1,49 @@
+const {CityRepository} = require('../repository/index');
+
+class CityServices{
+    constructor(){
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(data){
+        try {
+            const city = this.cityRepository.createCity(data)
+            return city;
+        } catch (error) {
+            console.log("Something wrong in Service layer");
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const city = this.cityRepository.deleteCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something wrong in Service layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId,data) {
+        try {
+            const city = this.cityRepository.updateCity(cityId,data)
+            return city;
+        } catch (error) {
+            console.log("Something wrong in Service layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city  = this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something wrong in Service layer");
+            throw {error};
+        }
+    }
+}
+
+module.exports = CityServices;
