a7f3cca8dbc602cda16679b9de6401b1e76025d3
VENOMJ-P <jay63837@gmail.com>
2024-10-01 18:15:22 +0530

    update routes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const { PORT } = require("./config/serverConfig.js");
+const ApiRoutes = require("./routes/index.js");
 
-const {PORT} = require("./config/serverConfig.js")
-const ApiRoutes = require('./routes/index.js');
+const db = require("./models/index.js");
+const { Airplane } = require("./models/index.js");
 
+const setupStartServer = async () => {
+  const app = express();
 
-const db = require('./models/index.js');
-const {Airplane} = require('./models/index.js')
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/searchService/api", ApiRoutes);
 
-const setupStartServer = async ()=>{
-    const app = express();
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
-    app.use('/api',ApiRoutes);
-
-    app.listen(PORT,async ()=>{
-        console.log(`Successfully server started at ${PORT}`);
-        // if(process.env.SYNC_DB){
-        //     db.sequelize.sync({alter:true});
-        // }
-
-        
-    });
-}
+  app.listen(PORT, async () => {
+    console.log(`Successfully server started at ${PORT}`);
+    // if(process.env.SYNC_DB){
+    //     db.sequelize.sync({alter:true});
+    // }
+  });
+};
 
 setupStartServer();
